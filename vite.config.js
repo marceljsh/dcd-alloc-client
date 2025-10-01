7120729a7d949c2e571fccd91b153400a81f7d83
ayususilowatii <arunikaptrii@gmail.com>
2025-10-01 18:15:35 +0700

    modified package.json and reset vitest test

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  test: {
+    environment: 'jsdom',
+    setupFiles: './vitest.setup.js',
+    globals: true,
+    alias: {
+      '@/': new URL('./src/', import.meta.url).pathname,
+    },
+  },
+});
